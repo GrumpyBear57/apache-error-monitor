0334c5ef0966a9c61c83aee2d2fe19cfabb04af6
GrumpyBear57 <gb57mail@yahoo.ca>
2016-05-13 10:54:43 -0600

    added more colours to output
    mainly for PHP errors, as I come across more, I will add them.
    Additionally you can add a pull request to implement them, or create an issue and I will look into it.

--- apache-error-monitor.sh
+++ apache-error-monitor.sh
@@ -1,6 +1,7 @@
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 NC='\033[0m'
-errorFile='[DIRECTORY TO YOUR ERROR FILE (eg /var/log/apache2/error.log)]'
+errorFile='/var/log/apache2/error.log'
 
 while inotifywait -q -e modify $errorFile >/dev/null; do
 	linesBefore=$lines
@@ -20,7 +21,14 @@ while inotifywait -q -e modify $errorFile >/dev/null; do
 	until [ "$checkedLines" = "$entriesInArray" ]; do
 		let "lineToRead += 1"
 		lineText=$(sed "${lineToRead}q;d" $errorFile)
-		printf "${RED}$lineText${NC}\n"
+		if [[ $lineText == *"PHP Fatal error"* ]]; then
+			printf "${RED}$lineText${NC}\n"
+		elif [[ $lineText == *"PHP Warning"* ]]; then
+			printf "${YELLOW}$lineText${NC}\n"
+		else
+			printf "$lineText\n"
+		fi
+		#printf "${RED}$lineText${NC}\n"
 		let "checkedLines += 1"
 	done
 	lines=$(awk 'END {print NR}' $errorFile)
